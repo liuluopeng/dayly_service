6096c7a847fd96faf2d012f27d52cd0455e8bc9a
llp <llp@lxllm.local>
2026-07-20 20:19:51 +0800

    fix: root build.gradle.kts 对所有子模块强制设置 Kotlin JVM target=17

--- kongde/android/build.gradle.kts
+++ kongde/android/build.gradle.kts
@@ -3,6 +3,9 @@ allprojects {
         google()
         mavenCentral()
     }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = "17"
+    }
 }
 
 val newBuildDir: Directory =
