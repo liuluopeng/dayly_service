c3ef190cffbbaebd8e3b5cf95e9388de513646e0
llp <llp@lxllm.local>
2026-07-20 16:15:27 +0800

    fix: Rust 日志推送 Flutter 时剥离 ANSI 转义码

--- kongde/rust/src/api/logger_bridge.rs
+++ kongde/rust/src/api/logger_bridge.rs
@@ -1,6 +1,6 @@
 use crate::frb_generated::StreamSink;
-use flutter_rust_bridge::frb;
 use std::sync::RwLock;
+use strip_ansi_escapes::strip;
 static LOGGER: RwLock<Option<StreamSink<String>>> = RwLock::new(None);
 
 #[flutter_rust_bridge::frb(sync)]
@@ -14,11 +14,12 @@ pub fn init_rust_logger(sink: StreamSink<String>) {
 
 #[flutter_rust_bridge::frb(ignore)]
 pub fn log_to_dart(msg: String) {
+    let cleaned = String::from_utf8_lossy(&strip(msg.as_bytes())).to_string();
     let logger: std::sync::RwLockReadGuard<Option<StreamSink<String>>> = match LOGGER.read() {
         Ok(val) => val,
         Err(val) => val.into_inner(),
     };
     if let Some(logger) = logger.as_ref() {
-        let _ = logger.add(msg);
+        let _ = logger.add(cleaned);
     }
 }
