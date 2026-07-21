7e83234d7548eaa06e2fce63d638d5581af17b4f
llp <llp@lxllm.local>
2026-07-21 12:26:05 +0800

    fix: 导出 TileSize 枚举

--- packages/flutter_metro_ui/lib/flutter_metro_ui.dart
+++ packages/flutter_metro_ui/lib/flutter_metro_ui.dart
@@ -1,6 +1,6 @@
 library flutter_metro_ui;
 
 export 'src/wp10_theme.dart';
-export 'src/tiles.dart' show MetroTile;
+export 'src/tiles.dart' show MetroTile, TileSize;
 export 'src/pivot.dart';
 export 'src/hub.dart';
